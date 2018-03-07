205bdebf6a79805adfe484b29f3e0451146b7e28
andrewdieken <dieke102@mail.chapman.edu>
2018-03-07 09:42:28 -0800

    Seed.rd file update

--- csafe-app/db/seeds.rb
+++ csafe-app/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Student
+User.create[first_name:"Andrew", last_name:"Dieken", email:"dieke102@mail.chapman.edu", user_name:"dieke102", password:"chapman2018", role:"Student"]
+#Student
+User.create[first_name:"Megan", last_name:"Yu", email:"yu322@chapman.edu", user_name:"yu322", password:"320CSafe!", role:"Student"]
+#Driver
+User.create[first_name:"Andre", last_name:"Perkins", email:"perki110@chapman.edu", user_name:"perki110", password:"Playstation@32", role:"Driver"]
+#Admin
+User.create[first_name:"Rene", last_name:"German", email:"german@chapman.edu", user_name:"germa101", password:"@IlikeJordanshoes@", role:"Admin"]
