d05c0d4978de942ab2d6318f49be8180d49564e2
Xavi Ablaza <xlablaza@gmail.com>
2018-03-07 07:52:11 -0800

    Added roles to users, added migrations

--- csafe-app/app/models/user.rb
+++ csafe-app/app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:rider, :driver, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :rider
+  end
+
 end
