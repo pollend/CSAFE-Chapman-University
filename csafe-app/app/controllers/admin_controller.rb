ba26faca63282c5e3eb4095ea6d59f3ec47f0e83
Xavi Ablaza <xlablaza@gmail.com>
2018-04-01 19:08:40 -0700

    Updated schema to support multiple roles for multiple users

--- csafe-app/app/controllers/admin_controller.rb
+++ csafe-app/app/controllers/admin_controller.rb
@@ -3,8 +3,10 @@ class AdminController < ApplicationController
   before_action :isAdminAndSignedIn
 
   def isAdminAndSignedIn
-    if not user_signed_in?
-      redirect_to new_user_session_path
+    if user_signed_in?
+      unless current_user.has_role? :admin
+        redirect_to new_user_session_path
+      end
     end
   end
 
@@ -15,7 +17,6 @@ class AdminController < ApplicationController
   def admin
     @hours = BusinessHour.all
     @rides = UserRide.all
-    @acceptedRides = AcceptedRide.all
   end
 
 end
--- csafe-app/db/migrate/20180322083608_create_user_permissions.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserPermissions < ActiveRecord::Migration[5.1]
-  def change
-    create_table :user_permissions do |t|
-
-      t.timestamps
-    end
-  end
-end
