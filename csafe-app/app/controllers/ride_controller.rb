d4190a3cc365bd90bfd37c044c2745d080bc399d
Xavi Ablaza <xlablaza@gmail.com>
2018-04-20 22:43:44 -0700

    Fixed bug where paths can be accessed without logging in

--- csafe-app/app/controllers/ride_controller.rb
+++ csafe-app/app/controllers/ride_controller.rb
@@ -3,10 +3,8 @@ class RideController < ApplicationController
   before_action :makeSureRiderAndSignedIn
 
   def makeSureRiderAndSignedIn
-    if user_signed_in?
-      unless current_user.has_role? :rider
-        redirect_to new_user_session_path
-      end
+    unless user_signed_in?
+      redirect_to unauthenticated_root_path
     end
   end
 
