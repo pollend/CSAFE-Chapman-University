4a0385474791382aac309a70fe5624e9a2f2f097
Xavi Ablaza <xlablaza@gmail.com>
2018-05-01 00:21:28 -0700

    Fix admin login route

--- csafe-app/app/controllers/login_controller.rb
+++ csafe-app/app/controllers/login_controller.rb
@@ -2,7 +2,7 @@ class LoginController < ApplicationController
 	def index
 		if user_signed_in?
 			if current_user.has_role? :admin
-				redirect_to "/admin/bounds"
+				redirect_to "/admin"
 			elsif current_user.has_role? :rider
 				redirect_to "/ride/request_ride"
 			end
