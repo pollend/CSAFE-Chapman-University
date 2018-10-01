6b9a55f71d062ca815957aff2f6275cb6118d321
Xavi Ablaza <xlablaza@gmail.com>
2018-09-30 18:56:33 -0700

    Rename files from app/assets into app/javascript
    Remove jquery3 from application.js
    
    Remove unused .coffee files
    
    Remove vendor files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
 //= require rails-ujs
 //= require turbolinks
-//= require_tree .
 //= require Chart.bundle
 //= require chartkick
+//= require_tree .
