dd07d389c2f08a9a0a6a2ec169aa4618ca865f11
Michael Pollind <polli104@mail.chapman.edu>
2018-10-03 12:05:04 -0700

    react stuff

--- app/javascript/packs/controllers/navbar_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from 'stimulus'
-
-export default class extends Controller {
-  static targets = ["menu"]
-
-  show() {
-    this.menuTarget.classList.toggle("is-active")
-  }
-}
